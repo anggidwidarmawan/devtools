fe51073dbb245f3b0447c4ed1969c2e17e085a70
anggi dwi darmawan <pratamaanggi090@gmail.com>
2022-05-24 09:23:41 +0700

    membuat devtools

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:aplikasi_devtools/three.dart';
+import 'package:aplikasi_devtools/two.dart';
+import 'package:aplikasi_devtools/widget_one.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Menu());
+}
+
+class MenuItem extends StatelessWidget {
+  const MenuItem(this.icon, this.itemText);
+  final String icon;
+  final String itemText;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Text(
+        icon,
+        style: TextStyle(
+          fontSize: 30.0,
+        ),
+      ),
+      title: Text(itemText),
+    );
+  }
+}
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Menu Demo'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              // Modify code here
+              Example1(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
